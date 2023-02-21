1b1f0f2c6c43df7b4c41a2993d9f04125cb2f533
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2023-02-21 22:20:05 +0200

    implement AppShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,14 @@
-<script>
+<script lang="ts">
 	// theme names: skeleton rocket vintage modern sahara seafoam hamlindigo gold-nouveau crimson
 	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.css';
 
-	import { Modal, Toast, drawerStore, ProgressRadial } from '@skeletonlabs/skeleton';
+	import { Modal, Toast, AppShell, drawerStore, ProgressRadial } from '@skeletonlabs/skeleton';
 
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Menu } from '@steeze-ui/remix-icons';
 
-	// import { storeDrawer } from '$lib/stores';
 	import AppDraw from '$lib/components/AppDraw.svelte';
 
 	import { page, navigating } from '$app/stores';
@@ -20,24 +19,26 @@
 	<meta name="description" content="Pasterr" />
 </svelte:head>
 
-<Modal />
+<Modal buttonTextCancel="Close" />
 <Toast position="tr" duration={500} />
 
 <AppDraw />
 
-<main class="h-screen scrollbar-thin relative">
+<!-- <main class="h-screen scrollbar-thin relative"></main> -->
+<AppShell>
 	{#if !$page.url.pathname.includes('raw')}
 		<button
 			type="button"
 			class="btn-icon absolute top-3"
 			on:click={() => {
-				drawerStore.open({});
+				drawerStore.open();
 			}}
 		>
 			<span> <Icon src={Menu} theme="solid" class="text-white" size="25" /> </span>
 		</button>
 	{/if}
 	{#if $navigating}
+		<!-- TODO maybe implement skeleton loading animation -->
 		<div class="h-full w-full flex flex-col py-5 justify-center items-center">
 			<div class="h-16 w-16">
 				<ProgressRadial stroke={90} />
@@ -46,4 +47,4 @@
 	{:else}
 		<slot />
 	{/if}
-</main>
+</AppShell>
